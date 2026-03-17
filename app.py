7590e968dd3c2e49234257d5f02752eb06cd0320
Kunal Anand <161216758+KunalAnand7222@users.noreply.github.com>
2026-03-17 10:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+import streamlit as st
 
 movies = pickle.load(open("movies.pkl","rb"))
 st.write(movies.columns)
